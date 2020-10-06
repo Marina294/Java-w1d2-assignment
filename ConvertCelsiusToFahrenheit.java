e1d9a3c1d75f18a41389faa0abf0d4b3bfe519f4
Marina294 <69162947+Marina294@users.noreply.github.com>
2020-10-06 11:28:12 -0700

    first commit

---
+++ ConvertCelsiusToFahrenheit.java
@@ -0,0 +1,41 @@
+// (Convert Celsius to Fahrenheit) Write a program that 
+// reads a Celsius degree in a double value from the console, 
+// then converts it to Fahrenheit and displays the result. 
+// The formula for the conversion is as follows:
+//          fahrenheit = (9 / 5) * celsius + 32
+// Hint: In Java, 9 / 5 is 1, but 9.0 / 5 is 1.8.
+
+class ConvertCelsiusToFahrenheit {
+
+    public static void main(String[] args) {
+        float fahrenheit, celsius;
+        celsius = 20;
+        fahrenheit = ((celsius * 9 / 5) + 32);
+        System.out.println("Fahrenheit is " + fahrenheit);
+
+    }
+}
+
+
+
+
+
+
+
+// function cToF(celsius) 
+// {
+//   let cTemp = celsius;
+//   let cToFahr = cTemp * 9 / 5 + 32;
+//   let message = cTemp+'\xB0C is ' + cToFahr + ' \xB0F.';
+//     console.log(message);
+// }
+
+// function fToC(fahrenheit) 
+// {
+//   let fTemp = fahrenheit;
+//   let fToCel = (fTemp - 32) * 5 / 9;
+//   let message = fTemp+'\xB0F is ' + fToCel + '\xB0C.';
+//     console.log(message);
+// } 
+// cToF(41);
+// fToC(32);
---
+++ FindTheNumberOfYears.java
@@ -0,0 +1,4 @@
+// (Find the number of years) Write a program 
+// that prompts the user to enter the minutes (e.g., 1 billion), 
+// and displays the number of years and days for the min- utes. 
+// For simplicity, assume a year has 365 days
