abae05f730672fb8e3fb5a05ddefd6b1478a27c3
Marina294 <69162947+Marina294@users.noreply.github.com>
2020-10-07 03:06:04 -0700

    question3

--- FinancialApplication.java
+++ FinancialApplication.java
@@ -3,3 +3,20 @@
 // rate, then computes the gratuity and total. For example, 
 // if the user enters 10 for subtotal and 15% for gratuity rate, 
 // the program displays $1.5 as gratuity and $11.5 as total. 
+
+public class FinancialApplication {
+
+    public static void main (String[] args) {
+        // float subtotal, gratuity, total;
+        // subtotal = 10;
+        // gratuity = subtotal + 0.15;
+        // total = subtotal + gratuity;
+
+        float f = 0.15f;
+        int subtotal = 10;
+        float total = subtotal + (subtotal * f);
+        
+        System.out.println("total is " + total);
+
+    }
+}
--- FindTheNumberOfYears.java
+++ FindTheNumberOfYears.java
@@ -1,4 +1,22 @@
+import java.util.Scanner;
+
 // (Find the number of years) Write a program 
 // that prompts the user to enter the minutes (e.g., 1 billion), 
 // and displays the number of years and days for the min- utes. 
 // For simplicity, assume a year has 365 days
+
+public class FindTheNumberOfYears {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("enter > ");
+        String input_text = scanner.nextLine();
+        System.out.println(input_text + "nyuuryoku");
+        scanner.close();
+
+
+    }
+
+}
